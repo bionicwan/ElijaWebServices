113583bbe2ba13a19f7dd7ecc383c76ae0a5f224
bionicwan <juancho989@gmail.com>
2014-01-20 13:09:03 -0600

    Se agrega validación de Token de acceso.

---
+++ Freakybite.ElijaWebServices.Processing/Helpers/ValidationHelper.cs
@@ -0,0 +1,45 @@
+﻿namespace Freakybite.ElijaWebServices.Processing.Helpers
+{
+    using System;
+
+    using Freakybite.ElijaWebServices.DataAccess.Repositories.Implementations;
+
+    public class ValidationHelper
+    {
+        #region Fields
+
+        private readonly DbContextFactory context = new DbContextFactory();
+        private UserRepository userRepository;
+
+        #endregion
+
+        #region Public Properties
+
+        public UserRepository UserRepository
+        {
+            get
+            {
+                if (this.userRepository == null)
+                {
+                    this.userRepository = new UserRepository(this.context);
+                }
+
+                return this.userRepository;
+            }
+        }
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        public bool ValidateToken(string token)
+        {
+            var userToken = new Guid(token);
+            var user = this.UserRepository.FindFirstBy(e => e.Token == userToken);
+
+            return user != null;
+        }
+
+        #endregion
+    }
+}
